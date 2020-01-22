00de755693961b7012c8c2b75d6438c0d1bb4fd1
Alex Russell <alexr@keytelematics.com>
2020-01-22 12:58:31 +0200

    Change seperator to _

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo Getting instance id
-export HOST_NAME=$HOST_NAME"."$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+export HOST_NAME=$HOST_NAME"_"$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 echo Setting Host Name to $HOST_NAME
 exec start_container "$@"
